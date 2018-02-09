d92933cf528fe8175518f299ba5810215b94fdfa
Daniel Esposito <daniel.esposito@wework.com>
2018-02-09 00:46:13 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
 # Write your #display_rainbow method here
+def #display_rainbow(one,two,three,four,five,si,seven)
+puts "R: #{one}, O: #{two}, Y: #{three}, G: #{four}, B: #{five}, I: #{six}, V: #{seven}"
+end
+
+
